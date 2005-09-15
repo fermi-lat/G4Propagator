75ca4aaef2b3f7f14a710f3f7f9b7ff5a2faab10
usher <>
2005-09-15 16:34:06 +0000

    Add check on valid volume for each step. Should fix JIRA issue GEANT-5

--- src/ParticleTransporter.cxx
+++ src/ParticleTransporter.cxx
@@ -4,7 +4,7 @@
 // @author Tracy Usher
 //
 // File and Version Information:
-// $Header: /nfs/slac/g/glast/ground/cvs/G4Propagator/src/ParticleTransporter.cxx,v 1.18 2005/07/13 22:22:47 usher Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/G4Propagator/src/ParticleTransporter.cxx,v 1.19 2005/07/15 21:55:30 usher Exp $
 //
 
 #include "ParticleTransporter.h"
@@ -279,9 +279,19 @@ bool ParticleTransporter::StepAnArcLength(const double maxArcLen)
             // Create a point which we hope is "just over the boundary"
             G4ThreeVector overPoint = curPoint + stepOverDist * curDir;
 
-            //Use the G4 navigator to locate the current point, then compute the distance
-            //to the volume boundary
+            //Use the G4 navigator to locate the current point
             G4VPhysicalVolume* pCurVolume    = navigator->LocateGlobalPointAndSetup(overPoint, 0, true, false);
+
+            // Let's be sure that we are inside a valid volume (ie inside of GLAST - it can happen!)
+            if (!pCurVolume) 
+            {
+                std::stringstream errorStream;
+                errorStream << "StepAnArcLength cannot find position within GLAST. pos: " 
+                    << overPoint << " dir: " << curDir << " step: " << stepInfo.size();
+                throw std::domain_error(errorStream.str());
+            }
+
+            // If ok, compute the distance to the volume boundary
             G4AffineTransform  globalToLocal = navigator->GetGlobalToLocalTransform();
             double             trackLen      = navigator->ComputeStep(overPoint, curDir, maxStep, safeStep) + stepOverDist;
 
