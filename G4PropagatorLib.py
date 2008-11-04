cc2920679a5e499c15ea0315a0eaa55450fa5ebf
ecephas <>
2008-11-04 15:22:04 +0000

    Added env.Tool('G4GeneratorLib')

--- G4PropagatorLib.py
+++ G4PropagatorLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/G4Propagator/G4PropagatorLib.py,v 1.1 2008/08/15 21:42:35 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['G4Propagator'])
-
+    env.Tool('G4GeneratorLib')
 def exists(env):
     return 1;
